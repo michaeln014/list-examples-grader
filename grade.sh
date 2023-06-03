f1a7a400817e598d7d194ef2aca2a9f507d7ccb3
michaeln014 <michaelnguyen1475065@gmail.com>
2023-06-03 00:14:01 -0700

    changes made in week 6

--- grade.sh
+++ grade.sh
@@ -14,3 +14,18 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+if [[ -f student-submission/ListExamples.java ]]
+then
+    cp student-submission/ListExamples.java grading-area
+else
+    echo File not found
+    exit 1
+fi
+
+cp TestListExamples.java grading-area
+cp GradeServer.java grading-area
+
+cp -r lib grading-area
+cd grading-area
+
+javac -cp '.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar' *.java
